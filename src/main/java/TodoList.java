e9413c59f3ef941c22920e556928e94b1b2296c8
nick <lopes.nick0801@gmail.com>
2021-11-04 17:08:24 -0400

    Skeleton Code

---
+++ src/main/java/TodoList.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+import java.io.*;
+import java.util.*;
+
+public class TodoList {
+    protected String Title;
+    protected ArrayList<TodoItem> list = new ArrayList<>();
+    protected void  checkTitle(){}
+    protected void  addItem(){}
+    protected void  removeItem(){}
+    protected void  editList(){}
+    protected void  editItem(){}
+    protected void  saveList(){}
+    protected void  loadList(){}
+    protected void  markComplete(){}
+    protected void  unMarkComplete(){}
+}
